7a735ff58580e71e27d1df2796f6cccfd9067a06
Dextersmith <dexterwinther@gmail.com>
2016-10-04 22:16:51 +0200

    Add files via upload

---
+++ NavBar.js
@@ -0,0 +1,9 @@
+document.getElementById("navMenu").innerHTML =
+'<ul>'+
+  '<p class="NameB1"> BRING IT. </p>'+
+  '<img class="BringLogo1" src="https://i.imgur.com/vmrBdRm.png" alt="Bring">'+
+  '<li class="active"><a href="index.html">HJEM</a></li>'+
+  '<li><a href="#">OM OSS</a></li>'+
+  '<li><a href="#">KONTAKT OSS</a></li>'+
+  '<li><a href="#">TJENESTER</a></li>'+
+'</ul>';
